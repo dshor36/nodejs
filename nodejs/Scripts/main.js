c5d1c68d65f2c6e351c9730815970eb4220781e4
dshor36 <dan_shor@hotmail.com>
2017-03-02 15:22:21 -0500

    Work done up to including file system step
    Work done up to including file system step

--- nodejs/Scripts/main.js
+++ nodejs/Scripts/main.js
@@ -58,7 +58,7 @@ eventEmitter.emit('connection');
 
 console.log("Program Ended.");*/
 
-var fs = require("fs");
+/*var fs = require("fs");
 
 fs.readFile('../Content/input.txt', function (err, data) {
     if (err) {
@@ -67,4 +67,223 @@ fs.readFile('../Content/input.txt', function (err, data) {
     }
     console.log(data.toString());
 });
-console.log("Program Ended");
+console.log("Program Ended");*/
+
+/*var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+// listener #1
+var listner1 = function listner1() {
+    console.log('listner1 executed.');
+}
+
+// listener #2
+var listner2 = function listner2() {
+    console.log('listner2 executed.');
+}
+
+// Bind the connection event with the listner1 function
+eventEmitter.addListener('connection', listner1);
+
+// Bind the connection event with the listner2 function
+eventEmitter.on('connection', listner2);
+
+var eventListeners = require('events').EventEmitter.listenerCount
+   (eventEmitter, 'connection');
+console.log(eventListeners + " Listner(s) listening to connection event");
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+// Remove the binding of listner1 function
+eventEmitter.removeListener('connection', listner1);
+console.log("Listner1 will not listen now.");
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+console.log(eventListeners + " Listner(s) listening to connection event");
+
+console.log("Program Ended.");*/
+
+/*buf = new Buffer(256);
+len = buf.write("Simply Easy Learning");
+
+console.log("Octets written : " + len);
+
+console.log("-----------------------------------------------");
+
+buf = new Buffer(26);
+for (var i = 0 ; i < 26 ; i++) {
+    buf[i] = i + 97;
+}
+
+console.log(buf.toString('ascii')); 
+console.log(buf.toString('ascii', 0, 5));
+console.log(buf.toString('utf8', 0, 5));
+console.log(buf.toString(undefined, 0, 5));
+
+console.log("-----------------------------------------------");
+
+var buf = new Buffer('Simply Easy Learning');
+var json = buf.toJSON(buf);
+
+console.log(json);
+
+console.log("-----------------------------------------------");
+
+var buffer1 = new Buffer('TutorialsPoint ');
+var buffer2 = new Buffer('Simply Easy Learning');
+var buffer3 = Buffer.concat([buffer1, buffer2]);
+console.log("buffer3 content: " + buffer3.toString());
+
+console.log("-----------------------------------------------");
+
+var buffer1 = new Buffer('ABC');
+var buffer2 = new Buffer('ABCD');
+var result = buffer1.compare(buffer2);
+
+if (result < 0) {
+    console.log(buffer1 + " comes before " + buffer2);
+} else if (result == 0) {
+    console.log(buffer1 + " is same as " + buffer2);
+} else {
+    console.log(buffer1 + " comes after " + buffer2);
+}
+
+console.log("-----------------------------------------------");
+
+var buffer1 = new Buffer('ABC');
+
+//copy a buffer
+var buffer2 = new Buffer(3);
+buffer1.copy(buffer2);
+console.log("buffer2 content: " + buffer2.toString());
+
+console.log("-----------------------------------------------");
+
+var buffer1 = new Buffer('TutorialsPoint');
+
+//slicing a buffer
+var buffer2 = buffer1.slice(0, 9);
+console.log("buffer2 content: " + buffer2.toString());
+
+console.log("-----------------------------------------------");
+
+var buffer = new Buffer('TutorialsPoint');
+
+//length of the buffer
+console.log("buffer length: " + buffer.length);
+
+console.log("-----------------------------------------------");*/
+
+
+/*var fs1 = require("fs");
+var data = '';
+
+// Create a readable stream
+var readerStream = fs1.createReadStream('../Content/input.txt');
+
+// Set the encoding to be utf8. 
+readerStream.setEncoding('UTF8');
+
+// Handle stream events --> data, end, and error
+readerStream.on('data', function (chunk) {
+    data += chunk;
+});
+
+readerStream.on('end', function () {
+    console.log(data);
+});
+
+readerStream.on('error', function (err) {
+    console.log(err.stack);
+});
+
+console.log("Program Ended");*/
+
+
+/*var fs2 = require("fs");
+var data = 'Simply Easy Learning';
+
+// Create a writable stream
+var writerStream = fs2.createWriteStream('../Content/output.txt');
+
+// Write the data to stream with encoding to be utf8
+writerStream.write(data, 'UTF8');
+
+// Mark the end of file
+writerStream.end();
+
+// Handle stream events --> finish, and error
+writerStream.on('finish', function () {
+    console.log("Write completed.");
+});
+
+writerStream.on('error', function (err) {
+    console.log(err.stack);
+});
+
+console.log("Program Ended");*/
+
+/*var fs = require("fs");
+
+// Create a readable stream
+var readerStream = fs.createReadStream('../Content/input.txt');
+
+// Create a writable stream
+var writerStream = fs.createWriteStream('../Content/output.txt');
+
+// Pipe the read and write operations
+// read input.txt and write data to output.txt
+readerStream.pipe(writerStream);
+
+console.log("Program Ended");*/
+
+/*var fs = require("fs");
+var zlib = require('zlib');
+
+// Compress the file input.txt to input.txt.gz
+fs.createReadStream('../Content/input.txt')
+   .pipe(zlib.createGzip())
+   .pipe(fs.createWriteStream('../Content/input.txt.gz'));
+
+console.log("File Compressed.");*/
+
+/*var fs = require("fs");
+var zlib = require('zlib');
+
+// Decompress the file input.txt.gz to input.txt
+fs.createReadStream('../Content/input.txt.gz')
+   .pipe(zlib.createGunzip())
+   .pipe(fs.createWriteStream('../Content/input.txt'));
+
+console.log("File Decompressed.");*/
+
+/*var fs = require("fs");
+
+// Asynchronous read
+fs.readFile('../Content/input.txt', function (err, data) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("Asynchronous read: " + data.toString());
+});
+
+// Synchronous read
+var data = fs.readFileSync('../Content/input.txt');
+console.log("Synchronous read: " + data.toString());
+
+console.log("Program Ended");*/
+
+var fs = require("fs");
+
+// Asynchronous - Opening File
+console.log("Going to open file!");
+fs.open('../Content/input.txt', 'r+', function (err, fd) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("File opened successfully!");
+});
