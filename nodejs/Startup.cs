4971857476ba9e5c0f1cb6543845d97107515fc7
dshor36 <dan_shor@hotmail.com>
2017-03-01 20:32:53 -0500

    Initial Check In
    This is the first checkin after creating a new solution in vs.
    Also, started tutorial and Finished up to the Node.Js - Event Loop step

---
+++ nodejs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nodejs.Startup))]
+namespace nodejs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
